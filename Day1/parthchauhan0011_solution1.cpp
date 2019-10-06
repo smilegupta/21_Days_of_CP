8cf436ec419f4fe49b27f6f992c48d4ae82dd457
Parth Chauhan <56200056+parthchauhan0011@users.noreply.github.com>
2019-10-06 11:43:26 +0530

    Solution for problem 1 Day 1
    Created solution for problem 1 day 1 by smile gupta.

---
+++ Day1/parthchauhan0011_solution1.cpp
@@ -0,0 +1,20 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+main()
+{
+
+	int a;
+	double b;
+	string c;
+	cin>>a;
+	cin>>b;
+	cin.ignore();
+	getline(cin,c);
+	cout<<a+4<<endl;
+	cout<<fixed;
+	cout<<std::setprecision(1)<<(b+4.0)<<endl;
+	cout<<"Hacktoberfest "<<c;
+	
+}
