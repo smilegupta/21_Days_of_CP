7419d56d51248a26cdb058874ecd5307cb8dc1a7
darkknight377 <51877339+darkknight377@users.noreply.github.com>
2019-10-06 11:44:11 +0530

    Solution for first problem
    It is c++ based solution for problem 1

---
+++ Day1/darkknight377_solution1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#include <stdio.h>
+using namespace std;
+int main() {
+  int a;
+  float b;
+  char c[30];
+  cin>>a;
+  cin>>b;
+  cout<<"\n";
+  gets(c);
+  cout<<a+int(b)<<endl;
+  cout<<2*b<<endl;
+  cout<<"Hacktoberfest ";
+  puts(c);
+  return 0;
+}
