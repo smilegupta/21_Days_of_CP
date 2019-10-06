7b892db4ba9e57323951ea51c84f489fc0231d78
jithukv143 <48850291+jithukv143@users.noreply.github.com>
2019-10-06 11:51:15 +0530

    Create file.cpp

---
+++ Day1/file.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#include<stdio.h>
+using namespace std;
+int main()
+{
+	int i;
+	int d=4;
+	float j,p;
+	char c[20];
+	cin>>i;
+	cin>>j;
+	cin>>c;
+	char e[]="hack";
+	p=j+d;
+	printf("%.1f",p);
+	cout<<"\n"<<i+d<<"\n";
+	strcat(e,c);
+	cout<<e<<"\n";
+	
+	
+	 
+}
