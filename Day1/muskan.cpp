1b264b42bbd2a601c3e9adbd61656e6e28504213
muskanmahajan37 <45875834+muskanmahajan37@users.noreply.github.com>
2019-10-06 12:00:02 +0530

    Create muskan.cpp

---
+++ Day1/muskan.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#include<stdio.h>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int a;
+    double b;
+    char c[20];
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    double d=4;
+    char e[]=" is the favorite fest of all developers.";
+    cout<<a+d<<"\n";
+    double k=(double) b+d;
+    cout<<fixed;
+    cout<<setprecision(1)<<k<<"\n";
+    strcat(c,e);
+    cout<<c<<"\n";
+    return 0;
+}
