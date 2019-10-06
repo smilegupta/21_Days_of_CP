01db7267242fbff8359a7161f31d373aadeded40
Shahbaz1234567 <47267636+Shahbaz1234567@users.noreply.github.com>
2019-10-06 11:45:54 +0530

    committed solution 01
    Hello world

---
+++ Day1/Shahbaz1234567_solution1.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+        int i=4;
+        double d=4.0;
+        string s="Hacktoberfest";
+        int a;
+        double b,e;
+        string c;
+        cin>>a>>b;
+        cin.ignore();
+        getline(cin,c);
+        e=(double)b+d;
+        cout<<i+a<<endl;
+        cout<<fixed;
+        cout<<std::setprecision(1)<<e<<endl;
+        cout<<s+c<<endl;
+	
+	
+}
