844e3cb329cc3f290c503f1760515c6f22d7e303
karanps2403 <55250375+karanps2403@users.noreply.github.com>
2019-10-06 11:47:37 +0530

    solution1.c
    Solution to problem 1 on day 1

---
+++ Day1/karanps2403_solution1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+	int x;
+	float y;
+	char z[50];
+	scanf("%d",&x);
+	scanf("%f",&y);
+	scanf("%s",&z);
+	printf("%d\n",x+4);
+	printf("%1f\n",y+4);
+	printf("hactoberfest %s",z);
+}
