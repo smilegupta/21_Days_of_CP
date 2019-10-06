66328e3172d1609935081430a4101355bc64e24f
rokibulislaam <rokibul630@gmail.com>
2019-10-06 11:43:39 +0530

    created solution

---
+++ RokibulIslaam_solution1.py
@@ -0,0 +1,10 @@
+input1 = int(input())
+input2 = float(input())
+input3 = input()
+
+sum = int(input1 + input2)
+sum_float = float(int(input2) + float(input2))
+
+print(sum)
+print(sum_float)
+print("Hactoberfest " + input3)
