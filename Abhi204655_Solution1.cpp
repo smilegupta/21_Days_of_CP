0e9d9d308e1477cd019d92f1f8e6ddc461cd353b
Abhi204655 <52554599+Abhi204655@users.noreply.github.com>
2019-10-06 11:46:42 +0530

    Solution for problem 1
    created solution for day 1 problem 1 by abhi kumar.

---
+++ Abhi204655_Solution1.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a;
+    float b;
+    string c;
+
+    cin>>a;
+    cin>>b;
+    cin.ignore();
+    getline(cin,c);
+    string z = "hactoberfest";
+    cout<<a+b<<endl;
+    cout<<a-b<<endl;
+    cout<<z+' '+c<<endl;
+    return 0;
+}
