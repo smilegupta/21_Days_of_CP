f921749c9136595c1e2e7ba4d7f2d046c412b2d3
venkatVAD <44518913+venkatVAD@users.noreply.github.com>
2019-10-06 11:48:23 +0530

    Create venkat_solution_for_1.cpp
    created solution for it

---
+++ Day1/venkat_solution_for_1.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int a,d=4;
+	string s="Hacktoberfest";
+	double b,e;
+	string c;
+	cin>>a;
+	cin>>b;
+	cin.ignore();
+	getline(cin,c);
+	e=(double)b+d;
+	cout<<d+a<<"\n";
+	cout<<fixed;
+	cout<<std::setprecision(1)<<e<<"\n";
+	cout<<s+c<<endl;
+}
