8c89a364fbb6e5891c34fc1c439a65d5cbb6b913
shivamp1998 <48366063+shivamp1998@users.noreply.github.com>
2019-10-06 11:52:03 +0530

    Create solution1_shivamp1998.cpp

---
+++ Day1/solution1_shivamp1998.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int a;
+	float b;
+	string c;
+	cin>>a>>b;
+	getchar();
+	getchar();
+	getline(cin,c);
+	cout<<a+b<<endl;
+	cout<<fixed<<setprecision(1)<<2.0*b<<endl;
+	cout<<"Hacktober"<<" "<<c<<endl;
+	
+}
