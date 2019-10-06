173267717fd71410c85ad6eadacf75a40872ae66
esdee-reign <56202218+esdee-reign@users.noreply.github.com>
2019-10-06 11:53:06 +0530

    esdee-reign_solution1.cpp
    solution problem1 day1

---
+++ Day1/esdee-reign_solution1.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+int i=4;
+double d = 4.0;
+string s = "Hacktobefest ";
+int a; 
+double b ,e;
+string c;
+cin>>a;
+cin>>b;
+cin.ignore();
+getline(cin,c);
+e = (double)b+d;
+cout<<i+a<<endl;
+cout<<fixed;
+cout<<std::setprecision(1)<<e<<endl;
+cout<<s+c<<endl;
+}
