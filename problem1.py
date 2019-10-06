aa1af6692dc855b5f1e527418bc166f01e9b5263
Aditya Banik <51051268+sharkfin777@users.noreply.github.com>
2019-10-06 11:43:10 +0530

    Create problem1.py

---
+++ problem1.py
@@ -0,0 +1,17 @@
+
+
+def prob1():
+    i=0
+    p=[]
+    while(i<3):
+        x=input()
+        p.append(x)
+        i+=1
+    print(int(p[0])+4)
+
+    print(float(p[1])+4)
+
+    print(p[2]+' is the favourite fest of all developers.')
+
+
+prob1()
