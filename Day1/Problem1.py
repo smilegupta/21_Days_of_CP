ddeb83b6231c7a6d8d4ff2c994e93fe697dda8ac
Ranjith-Ranjith <47072936+Ranjith-Ranjith@users.noreply.github.com>
2019-10-13 13:23:36 +0530

    Solution to Problem1.py

---
+++ Day1/Problem1.py
@@ -0,0 +1,15 @@
+#HactoberFest 
+# Day 1 - Problem 1 
+
+w = "Hactoberfest "
+
+x = int(input()) # String to int Type Conversion
+
+y = float(input()) # String to float Type Conversion
+
+z = input() # String input 
+
+
+print(x + int(y))
+print(2*y)
+print(w + z)
